d12afcb0929c2b61935847ba9dd7237d2ebed0e7
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-05-29 23:30:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def start(bot, update):
 @pr0fess0r_99.on_message(filters.private & filters.text & ~filters.command(["start"]))
 async def sharelink(bot, update):
     await bot.send_photo(chat_id=update.chat.id, photo=environ.get("BOT_PIC", "https://telegra.ph/file/2b82d3a491f6b5869092c.jpg"),
-        caption=f"Message Sharing Link Is Ready :- https://t.me/share/url?url={quote(update.text)}", reply_to_message_id=update.id
+        caption=f"**Message Sharing Link Is Ready** :- https://t.me/share/url?url={quote(update.text)}", reply_to_message_id=update.id, reply_markup=InlineKeyboardMarkup( [[ InlineKeyboardButton("📤 Share Link 📤", url=f"https://t.me/share/url?url={quote(update.text)}") ]] ))       
     )
 
 print("~~~~~~~~~~~")
